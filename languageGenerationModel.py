8a190d46c0886e4bf636250ae8b51127aa6939f8
some user <some@user.com>
2021-12-09 12:40:20 +0900

    - removed some old code

--- languageGenerationModel.py
+++ languageGenerationModel.py
@@ -33,7 +33,6 @@ import math
 
 class MethodGenerationModel(model.Model):
     def __init__(self):
-        self.dataset = None
         self.languageModeling = None
         self.languageGenerator = None
 
@@ -71,88 +70,6 @@ class MethodGenerationModel(model.Model):
 
     # Trains the model using the given training set
     def train_model(self, training_set: str) -> None:
-        #tokenizer = Tokenizer(WordPiece(unk_token='[UNK]'))
-        #trainer = WordPieceTrainer(special_tokens=['[UNK]','[CLS]','[SEP]','[PAD]','[MASK]','[MTD]','[/MTD]','[TYP]','[/TYP]','[PAR]','[/PAR]'])
-        #tokenizer.pre_tokenizer = Whitespace()
-        #tokenizer.train([training_set], trainer)
-        #tokenizer.save('customTokenizer.json')
-        #inputs = tokenizer(training_set, padding='max_length', truncation=True)
-        #m = AutoModel.from_pretrained('bert-base-uncased')
-        #args = TrainingArguments("test")
-        #t = Trainer(model=m, args=args, train_dataset=inputs)
-        #t.train()
-        #t.save_state()
-        '''tokenizer = Tokenizer(WordPiece())
-        tokenizer.pre_tokenizer = Whitespace()
-        tokenizer.add_special_tokens(['<MSP>', '<PSP>'])
-        tokenizer.train(files=[training_set])
-        tokenizer.save('customTokenizer.json')
-        mappedTokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer)
-        m = AutoModel.from_pretrained('bert-base-uncased')
-        args = TrainingArguments('test')
-        t = Trainer(model=m, args=args, tokenizer=mappedTokenizer, train_dataset=mappedTokenizer(self.dataset, padding='max_length', truncation=True))
-        t.train()
-        t.save_state()'''
-        '''
-        trainDf = pd.read_csv(training_set, header=None, names=['input_text', 'target_text'], sep=';', na_filter=False)
-        args = Seq2SeqArgs(num_train_epochs=5)
-        model = Seq2SeqModel('roberta', 'roberta-base', 'bert-base-uncased', args=args)
-        model.train_model(trainDf)
-        '''
-        '''
-        print('Load sequences:\n')
-        trainDf = pd.read_csv(training_set, header=None, names=['input_text', 'target_text'], sep=';', na_filter=False)
-        input_sequences = list()
-        output_sequences = list()
-        task_prefix = "get parameters for: "
-        for i, row in trainDf.iterrows():
-            input_sequences.append(task_prefix + row[0])
-            output_sequences.append(row[1])
-
-        #self.languageModeling.train_model(training_set)
-        print('Tokenize sequences:\n')
-        tokenizer = T5Tokenizer.from_pretrained('t5-small')
-        model = T5ForConditionalGeneration.from_pretrained('t5-small')
-        encoding = tokenizer(input_sequences, padding='longest', max_length=64, truncation=True, return_tensors='pt')
-        input_ids, attention_mask = encoding.input_ids, encoding.attention_mask
-
-        #encode targets
-        target_encodings = tokenizer(output_sequences, padding='longest', max_length=128, truncation=True)
-
-        print('Create labels:\n')
-        labels = target_encodings.input_ids
-
-        labels = [
-            [(label if label != tokenizer.pad_token_id else -100) for label in labels_example] for labels_example in labels
-        ]
-        labels = torch.tensor(labels)
-
-        rows = len(input_ids)
-        rowsPerStep = 64
-        steps = 50 #math.floor(rows / rowsPerStep + 1) 
-        print('Rows ' + str(rows) + ' / rows per step ' + str(rowsPerStep) + ' / steps ' + str(steps))
-        for i in range(steps):
-            print('Train model: Step #' + str(i))
-            inputs, attentions, ls = list(), list(), list()
-            if i*rowsPerStep >= rows:
-                break
-            elif steps*rowsPerStep >= rows and i == steps-1:
-                inputs, attentions, ls = input_ids[rowsPerStep*i:], attention_mask[rowsPerStep*i:], labels[rowsPerStep*i:]
-            else:
-                n = i+1
-                inputs, attentions, ls = input_ids[rowsPerStep*i:rowsPerStep*n], attention_mask[rowsPerStep*i:rowsPerStep*n], labels[rowsPerStep*i:rowsPerStep*n]
-            loss = model(input_ids=inputs, attention_mask=attentions, labels=ls).loss
-            print('Loss:\n' + str(loss))
-
-        print('Generate outputs:')
-        testMethod = task_prefix + 'find person by name'
-        testinputids = tokenizer(testMethod, return_tensors='pt').input_ids
-        outputs = model.generate(testinputids)
-        print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-        
-        tokenizer.save_pretrained(get_resource_path('customTokenizer'))
-        model.save_pretrained(get_resource_path('outputs'))
-        '''
         trainDf = pd.read_csv(training_set, header=None, names=['prefix', 'input_text', 'target_text'], sep=';', na_filter=False)
         args = T5Args()
         args.num_train_epochs = 3
@@ -171,19 +88,6 @@ class MethodGenerationModel(model.Model):
 
     # Makes predictions
     def predict(self, predictionData: list) -> list:
-        '''print('load tokenizer / mmodel:\n')
-        task_prefix = "get parameters for: "
-        tokenizer = T5Tokenizer.from_pretrained(get_resource_path('customTokenizer'))
-        model = T5ForConditionalGeneration.from_pretrained(get_resource_path('outputs'))
-        output = list()
-        for methodName in predictionData:
-            print("Generate for '"  + methodName + "'")
-            inputs = tokenizer(task_prefix + methodName, return_tensors='pt')
-            outputs = model.generate(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
-            #generated = self.languageGenerator.generate(methodName)[0]
-            print('generated: ' + str(tokenizer.decode(outputs[0])))
-            output.append(methodName)
-            '''
         model = T5Model('t5', 'outputs')
         inputs = list()
         for methodName in predictionData:
@@ -202,20 +106,6 @@ class MethodGenerationModel(model.Model):
 
     # formats input data
     def dataFormatter(self, data):
-        filename = 'lmd_' + str(uuid4())
-        filepath = get_resource_path(filename)
-        copyTo(data, filepath)
-        
-        formatted = list()
-        for line in data.readlines():
-            if (line[len(line)-1]) == '\n':
-                line = line[:len(line)-1]
-            formatted.append(line[:len(line)-2])
-        self.dataset = formatted
-        return filepath
-        #formatted = list()
-        #for line in data.readlines():
-        #    if (line[len(line)-1]) == '\n':
-        #        line = line[:len(line)-1]
-        #    formatted.append(line[:len(line)-2])
-        #return formatted
+        if not isinstance(data, pd.DataFrame):
+            return pd.read_csv(data, header=None, names=['prefix', 'input_text', 'target_text'], sep=';', na_filter=False)
+        return data
